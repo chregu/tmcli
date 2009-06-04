d7d51ea5290f3484f2230536e049450c7611b1be
Christian Stocker <me@chregu.tv>
2009-06-04 08:27:12 +0200

    initial work

---
+++ fse.c
@@ -0,0 +1,52 @@
+#include <CoreServices/CoreServices.h>
+#include <unistd.h>
+#include <stdio.h>
+
+void myCallbackFunction(
+    ConstFSEventStreamRef streamRef,
+    void *clientCallBackInfo,
+    size_t numEvents,
+    void *eventPaths,
+    const FSEventStreamEventFlags eventFlags[],
+    const FSEventStreamEventId eventIds[])
+{
+    int i;
+    char **paths = eventPaths;
+ 
+    // printf("Callback called\n");
+    for (i=0; i<numEvents; i++) {
+        int count;
+        /* flags are unsigned long, IDs are uint64_t */
+        printf("Change %llu in %s, flags %lu\n", eventIds[i], paths[i], eventFlags[i]);
+   }
+}
+
+int
+main(int argc, char *argv[])
+{
+    /* Define variables and create a CFArray object containing
+       CFString objects containing paths to watch.
+     */
+    CFStringRef mypath = CFSTR("/");
+    CFArrayRef pathsToWatch = CFArrayCreate(NULL, (const void **)&mypath, 1, NULL);
+    void *callbackInfo = NULL; // could put stream-specific data here.
+    FSEventStreamRef stream;
+    CFAbsoluteTime latency = 3.0; /* Latency in seconds */
+ 
+    /* Create the stream, passing in a callback, */
+    stream = FSEventStreamCreate(NULL,
+        &myCallbackFunction,
+        callbackInfo,
+        pathsToWatch,
+        atoi(argv[1]), /* Or a previous event ID */
+        latency,
+        kFSEventStreamCreateFlagNone /* Flags explained in reference */
+);
+
+    /* Create the stream before calling this. */
+    FSEventStreamScheduleWithRunLoop(stream, CFRunLoopGetCurrent(),         kCFRunLoopDefaultMode);
+	FSEventStreamStart(stream);
+	CFRunLoopRun();
+
+}
+
