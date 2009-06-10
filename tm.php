ae04f04ccc3fd945956016f3f3bee393d2b1e560
Christian Stocker <me@chregu.tv>
2009-06-10 15:31:51 +0200

    some refactoring

--- tm.php
+++ tm.php
@@ -3,56 +3,51 @@
 passthru('sudo -u chregu hdiutil attach -noautofsck -noverify /Volumes/My\ Book\ II/backup.sparsebundle');
 $logg = false;
 $full = false;
-include_once("settings.php");
-include_once("tmcli.php");
+include_once ("settings.php");
+include_once ("tmcli.php");
+
+
 
 $tm = new tmcli($outdir);
 
-$lastid = $tm->getLastId();
+$tm->dryrun = $dryrun;
+$tm->logg = $logg;
 
+$lastid = $tm->getLastId();
+$lastuuid = $tm->getLastUUID();
 if (!$lastid) {
     $full = true;
+    $lastid = 1;
 }
 
 $lastdir = $tm->getLastDir();
 $orilastid = $lastid;
-if (!$lastid) {
-    $lastid = 1;
-}
 
 $dirs = array();
 $newlastid = null;
-$rdirs = array();
+$rdirs = $tm->getExcludedPathsByApple();
 $rdirs2 = array();
 //excluded by apple
-$out =  `mdfind "com_apple_backup_excludeItem = 'com.apple.backupd'"`;
-$apple = explode("\n",trim($out));
 
-
-// to be implemented
-// defaults read /System/Library/CoreServices/backupd.bundle/Contents/Resources/StdExclusions ContentsExcluded
-
-foreach ($apple as $a) {
-    $rdirs[$a] = false;
-}
 
 while (!$newlastid) {
-    print $lastid ."\n";
+    print $lastid . "\n";
     //   print "la ". $lastid ."\n";
     $out = `./fse 1 $lastid`;
-    $events = explode("\n",trim($out));
+    $events = explode("\n", trim($out));
 
-    foreach($events as $e) {
+    foreach ($events as $e) {
+
         $e = trim($e);
         if ($e) {
-            $es = explode(":",$e,3);
+            $es = explode(":", $e, 3);
             if (isset($es[2])) {
-                $es[2] = "/" .$es[2];
+                $es[2] = "/" . $es[2];
             }
             if ($es[1] == 0) {
                 $lastid = $es[0];
                 $dirs[$es[2]] = 0;
-                if (substr($es[2],0,9) == "/Volumes/") {
+                if (substr($es[2], 0, 9) == "/Volumes/") {
                     continue;
                 }
             } else if ($es[1] & 1) {
@@ -69,43 +64,49 @@ while (!$newlastid) {
             } else if ($es[1] == 256) {
 
                 $devuuid = $es[0];
-                file_put_contents($outdir."/uuid.dat",$devuuid);
+                if ($lastuuid != $devuuid) {
+                    print $lastuuid."\n";
+                    print $devuuid."\n";
+                    $full = true;
+                    break;
+                }
 
             }
         }
     }
 
-
 }
-
 
 $lastid = $newlastid;
 ksort($dirs);
 $rdiffdat = "";
-foreach($dirs as $d => $q) {
+foreach ($dirs as $d => $q) {
 
-
-    $ds = explode("/",trim($d,"/"));
+    $ds = explode("/", trim($d, "/"));
     if ($ds[0] == 'Volumes') {
         continue;
     }
     $path = "/";
-    foreach ($ds as $k=> $p) {
-        $path .= $p ."/";
-        if (!isset($rdirs[$path]) || (isset($rdirs[$path."*/"]) && $rdirs[$path ."*/"] === false)) {
+    foreach ($ds as $k => $p) {
+        $path .= $p . "/";
+        if (!isset($rdirs[$path]) || (isset($rdirs[$path . "*/"]) && $rdirs[$path . "*/"] === false)) {
             $rdirs[$path] = true;
-            if (isset($rdirs[$path."*/"]) && $rdirs[$path ."*/"] === false) {
-                unset($rdirs[$path ."*/"]);
+            if (isset($rdirs[$path . "*/"]) && $rdirs[$path . "*/"] === false) {
+                unset($rdirs[$path . "*/"]);
 
             }
-            $rdirs2[$path."*"] = true;
-            $rdirs2 = array_merge(array($path."*/" => false),$rdirs2);
-
+            $rdirs2[$path . "*"] = true;
+            $rdirs2 = array_merge(array(
+                    $path . "*/" => false
+            ), $rdirs2);
 
         }
-        if ($k > $maxdir) { break;}
+        if ($k > $maxdir) {
+            break;
+        }
     }
     //$rdiffdat .= "- $path**\n";
+
 
     if ($k > $maxdir || $q & 1) {
         if (!isset($rdirs["$path**"])) {
@@ -114,12 +115,10 @@ foreach($dirs as $d => $q) {
     } else {
         if (!isset($rdirs["$path*/"])) {
             //       $rdirs["$path*/"] = false;
-            //     $rdirs["$path*"] = true;
+        //     $rdirs["$path*"] = true;
         }
     }
 }
-
-
 
 $rdiffdat = "";
 if ($full) {
@@ -128,64 +127,50 @@ if ($full) {
 } else {
     foreach ($rdirs as $k => $v) {
         if ($v) {
-            $rdiffdat .="+ ";
+            $rdiffdat .= "+ ";
         } else {
-            $rdiffdat .="- ";
+            $rdiffdat .= "- ";
         }
-        $rdiffdat .="$k\n";
+        $rdiffdat .= "$k\n";
     }
-    $rdiffdat .="#####\n";
+    $rdiffdat .= "#####\n";
     array_reverse($rdirs2);
 
     foreach ($rdirs2 as $k => $v) {
         if ($v) {
-            $rdiffdat .="+ ";
+            $rdiffdat .= "+ ";
         } else {
-            $rdiffdat .="- ";
+            $rdiffdat .= "- ";
         }
-        $rdiffdat .="$k\n";
+        $rdiffdat .= "$k\n";
     }
 
-     $rdiffdat .= "+ /*\n";
+    $rdiffdat .= "+ /*\n";
     $rdiffdat .= "- /**\n";
 
 }
-file_put_contents("./rdiff.dat",file_get_contents("./backuprdiff.dat").$rdiffdat);
+file_put_contents("./rdiff.dat", file_get_contents("./backuprdiff.dat") . $rdiffdat);
 
-$linkdir = "../".basename($lastdir);
-$cmd = "rsync $dryrun --out-format='%i %n%L' -x --human-readable -H -A -X -E -a  --link-dest=$linkdir --delete   --stats  --include-from=./rdiff.dat / $bkdir".".inProgress";
-print "Backing up ...\n";
-if ($logg) {
- ob_start();
+$tm->runRsync();
 
-}
-print $cmd ."\n";
-
-passthru($cmd);
 if (!$dryrun) {
-    print $lastid ."\n";
-    file_put_contents("$outdir/lastid.dat",$lastid);
-    file_put_contents("$bkdir.inProgress/lastid.dat",$lastid);
-
-    file_put_contents($bkdir.".inProgress/uuid.dat",$devuuid);
+    print $lastid . "\n";
+    $tm->setLastId($lastid);
+    $tm->setLastUUID($devuuid);
 
     $bkdir2 = $bkdir;
-    $bkdir = realpath($bkdir.".inProgress");
+    $bkdir = realpath($bkdir . ".inProgress");
     if (!$full) {
-    print "Create Links...   \n";
-    include("mklinks.php");
+        print "Create Links...   \n";
+        include ("mklinks.php");
     }
-    rename($bkdir,$bkdir2);
-    file_put_contents("$outdir/lastdir.dat",basename($bkdir2));
-    file_put_contents("$bkdir2/lastdir.dat",basename($bkdir2));
 
+    $tm->moveBkDir();
+
+    $tm->writeLastDir();
 }
 
 
-if ($logg) {
-$foo = ob_get_contents();
 
-file_put_contents($bkdir2."/tm.log",$foo);
 
-print $foo;
-}
+$tm->cleanUp();
